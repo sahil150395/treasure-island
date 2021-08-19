5f7c4b1e6f3eaa24d613a26a0c824af4686d9f23
Sahil Gulati <64752326+sahil150395@users.noreply.github.com>
2021-08-19 07:38:49 +0000

    passed

--- main.py
+++ main.py
@@ -24,3 +24,22 @@ print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.") 
 
 #https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+lft_rgt = input('You\'re at a cross road. Where do you want to go? Type "left" or "right"\n').lower()
+
+if lft_rgt == "left":
+    lake = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.\n').lower()
+    if lake == "wait":
+        door = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?\n").lower() 
+        if door == "red":
+            print("Burned by fire. Game Over.")
+        elif door == "yellow":
+            print("You Win!")
+        elif door == "blue":
+            print("Eaten by beasts. Game Over.")
+        else:
+            print("You chose a door that doesn't exist. Game Over.")
+    else:
+        print("Attacked by trout. Game Over.")
+else:
+    print("Fall into a hole. Game Over.")
